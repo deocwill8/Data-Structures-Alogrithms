58ace4978ed3d127b270304c356c2549337013d5
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2022-02-28 08:14:50 -0600

    updating gitignore and adding new problem class

---
+++ src/MinimumWaitingTime.kt
@@ -0,0 +1,13 @@
+class MinimumWaitingTime {
+
+    fun minimumWaitingTime(queries: MutableList<Int>): Int {
+        // Write your code here.
+        return -1
+    }
+
+}
+
+fun main() {
+    MinimumWaitingTime().minimumWaitingTime(mutableListOf())
+
+}
