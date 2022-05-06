19a07181f184986eec0ae38238321b72ac480d90
deocwill <deona.dev@gmail.com>
2022-05-06 08:57:11 -0500

    adding iterative solution

--- src/NthFibonacci.kt
+++ src/NthFibonacci.kt
@@ -1,23 +1,51 @@
 class NthFibonacci {
     companion object {
         //recursive
-//        fun updateCache(cache: MutableMap<Int, Int>) {
-//            cache.put(1,0)
-//            cache.put(2,1)
-//        }
+
+        //TODO figure out the cache way of solving it
+
         //simple
-        fun getNthFib(n: Int): Int {
-           if (n == 2) {
-               return 1
-           } else if (n == 1) {
-               return 0
-           }
-            return  getNthFib(n-1) + getNthFib(n - 2)
+//        fun getNthFib(n: Int): Int {
+//           if (n == 2) {
+//               return 1
+//           } else if (n == 1) {
+//               return 0
+//           }
+//            return  getNthFib(n-1) + getNthFib(n - 2)
+//        }
+
+        // iterative
+        // TODO write in less lines
+        fun getNthFib(n:Int): Int {
+            val lastTwo: MutableList<Int> = mutableListOf(0,1)
+            var counter: Int = 3
+            var nextFib: Int
+
+            while (counter <= n) {
+                nextFib = lastTwo[0] + lastTwo[1]
+                lastTwo[0] = lastTwo[1]
+                lastTwo[1] = nextFib
+                counter++
+            }
+            return when (n) {
+                1 -> {
+                    println("0")
+                    0
+                }
+                2 -> {
+                    println("1")
+                    1
+                }
+                else -> {
+                    println(lastTwo[1])
+                    lastTwo[1]
+                }
+            }
         }
     }
 }
 
 fun main() {
-    NthFibonacci.getNthFib(6)
+    NthFibonacci.getNthFib(5)
     //NthFibonacci.getNthFib(22)
 }
